f1376d99d87600214367a6b7c6410789f4e3da6f
Alex <160778418+Alex000115@users.noreply.github.com>
2026-01-18 13:40:55 +0600

    Create network.config.js

---
+++ network.config.js
@@ -0,0 +1,10 @@
+export const NETWORKS = {
+  ethereum: "https://rpc.ankr.com/eth",
+  base: "https://rpc.ankr.com/base",
+  polygon: "https://rpc.ankr.com/polygon",
+  arbitrum: "https://rpc.ankr.com/arbitrum"
+};
+
+export const NETWORK = "ethereum";
+export const TOKEN_ADDRESS = "0x0000000000000000000000000000000000000000";
+export const BENEFICIARY_ADDRESS = "0x0000000000000000000000000000000000000000";
